e4ea36b75756a92cb26e7f6bf47c678b9a1631da
phusit999-9 <phusitintech@gmail.com>
2025-04-18 20:26:37 +0700

    delete stripe key

--- application/controllers/Stripe.php
+++ application/controllers/Stripe.php
@@ -66,7 +66,6 @@ class Stripe extends CI_Controller {
 
      
 
-    //   $stripeSecret = 'sk_test_51GaDAnB5TcGYIInBQ0lO3ucMUze485kGRTBghAn8ordblplJrUy3a439LRqkLICpxAh3U0Id6Ua28c2r2Dg2Gt0a00RekNi3LH';
 
  
 
